a4b8b6af22e5c011887aca7e8efd0d82276ae68b
Nicolas Valencia <62915251+eduvaldev@users.noreply.github.com>
2021-04-14 20:34:13 -0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 80;
   await app.listen(port);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
